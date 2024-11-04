dbf148ebdb9ba27ba531c15e97bc7e72175f07b0
IanLiu2603 <ian.liu2603@gmail.com>
2024-11-04 09:33:53 -0800

    L16 button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 // todo
+const APP_NAME = "GeoCache";
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+document.title = APP_NAME;
+
+//L16 button
+const L16 = document.createElement("button");
+L16.addEventListener("click", () => alert("you clicked the button!"));
+app.append(L16);
